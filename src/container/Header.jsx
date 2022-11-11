82f19bcd9d69b82363c05b7d8111c035ae9617e9
sidpd57 <sidpd57@gmail.com>
2022-11-11 19:02:44 +0530

    Initial commit

---
+++ src/container/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand ps-4" href="#">
+          Mentor
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item ">
+              <a className="nav-link ">Home</a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link ">Contact</a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link ">Support</a>
+            </li>
+          </ul>
+        </div>
+        <form className="d-flex">
+            <button className="btn btn-outline-success ms-4 nav-item" type="button">
+              Login
+            </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
